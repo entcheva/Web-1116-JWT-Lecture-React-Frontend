adf2e4848a355606ad59467cb0e135531fd842de
JJ Seymour <jjseymour@gmail.com>
2017-02-13 16:25:49 -0500

    in class

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { browserHistory } from 'react-router'
 
 const URL = 'http://localhost:3000/api/v1/'
 
@@ -13,9 +14,22 @@ export const fetchDrinks = () => {
 
 export const fetchCategories = () => {
   let request = axios.get(URL + 'categories')
-  
+
   return {
     type: "FETCH_CATEGORIES",
     payload: request
+  }
+}
+
+export const createUser = (user) => {
+  const response = axios.post(URL + 'signup', user).then((userData) => {
+    sessionStorage.setItem('jwt', userData.data.jwt)
+    browserHistory.push("/drinks")
+    return userData
+  })
+
+  return {
+    type: 'CREATE_USER',
+    payload: response
   }
 }
